a0b54f9b0103b169f9f941842c260f4b37417565
wenheeluyu <302505253@qq.com>
2020-04-16 15:30:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,140 @@
+<template>
+  <div id="app" >
+    <el-container>
+      <el-header class="hdr">
+        <el-row :gutter="15">
+          <el-col :span="6"><div class="logo">小莱书城</div></el-col>
+          <el-col :span="18">
+            <div class="top_button">
+              <el-link :underline="false" @click="loginVisiable=true">登录</el-link> |
+              <el-link :underline="false" @click="registVisiable=true">注册</el-link> |
+              <el-link :underline="false" @click="aboutVisiable=true">关于</el-link>
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main class="ctn">
+        <div class="inner">
+          <router-view/>
+        </div>
+      </el-main>
+      <el-footer class="ftr">@ 格莱科技集团 版权所有 Support by 软件研发部</el-footer>
+    </el-container>
+    <!-- 登陆的弹出框 -->
+    <el-dialog :visible.sync="loginVisiable" @close="loginVisiable=false">
+      <el-form :model="loginForm">
+        <el-form-item label="账号" :label-width="formLabelWidth">
+          <el-input v-model="loginForm.acount" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="loginForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="loginVisiable = false">注册</el-button>
+      </span>
+    </el-dialog>
+    <!-- 注册的弹出框 -->
+    <el-dialog :visible.sync="registVisiable" @close="registVisiable=false">
+      <el-form :model="registForm">
+        <el-form-item label="账号" :label-width="formLabelWidth">
+          <el-input v-model="registForm.acount" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="registForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" :label-width="formLabelWidth">
+          <el-input v-model="registForm.cfmPsd" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="registVisiable = false">提交</el-button>
+      </span>
+    </el-dialog>
+    <!-- 关于的弹出框 -->
+    <el-dialog :visible.sync="aboutVisiable" @close="aboutVisiable=false">
+      <span>需要设置visible属性，它接收Boolean，当为true时显示 Dialog。Dialog 分为两个部分：body和footer，
+        footer需要具名为footer的slot。title属性用于定义标题，它是可选的，默认值为空。
+        最后，本例还展示了before-close的用法。
+        </span>
+    </el-dialog>
+
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      //登录
+      loginVisiable:false,
+      loginForm:{
+        acount:'',
+        password:''
+      },
+      //注册
+      registVisiable:false,
+      registForm:{
+        acount:'',
+        password:'',
+        cfmPsd:''
+      },
+      //关于
+      aboutVisiable:false,
+      //自定义label宽度
+      formLabelWidth:'80px'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
+$lh:60px;
+html,body{
+ height: 100%;
+}
+body{
+  margin: 0;
+  padding: 0;
+}
+#app{
+  height: 100%;
+  section{
+    height: 100%;
+  }
+}
+.hdr{
+  background: navy;
+  .logo{
+    line-height: $lh;
+    font-size: 40px;
+    color: #fff;
+  }
+  .top_button{
+    text-align: right;
+    line-height: $lh;
+    color: #fff;
+    span,a{
+      color: #fff;
+    }
+
+  }
+}
+.ctn{
+  background: oldlace;
+  .inner{
+    height: 100%;
+    overflow-y: auto;
+  }
+}
+.ftr{
+  background: gainsboro;
+  line-height: $lh;
+  text-align: center;
+  font-size: 12px;
+  color: gray;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="home">
+    <el-row :gutter="15">
+      <el-col :span="6">
+        <h1 class="tit">全部书籍</h1>
+        <div v-for='item in list' :key="item.id" class="ltBox">
+          <h3>{{item.title}}</h3>
+          <p v-for='sub in item.books' :key='sub.id' class="bookMenu">
+            {{sub.name}}
+          </p>
+        </div>
+      </el-col>
+      <el-col :span="18">
+        <div class="img"></div>
+        <!-- 卡片 -->
+         <el-row :gutter="10">
+          <el-col :span="12" v-for="(o) in cards" :key="o.id">
+            <el-card :body-style="{ padding: '0px' }">
+              <el-col :span='6'>
+                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" width="100%" class="image">
+              </el-col>
+              <el-col :span='18'>
+                <span class="cards_tit">{{o.title}}</span>
+                <p class="cards_text">{{o.text}}</p>
+                <el-button type="primary" size="mini" class="buybtn">立即购买</el-button>
+              </el-col>
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+    <MainPage />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  data(){
+    return{
+      list:[
+        {
+          id:1,
+          title:'移动互联网开发专业书籍',
+          books:[
+            {
+              id:'dlp1',
+              name:'web标准网页设计之道',
+            },
+            {
+              id:'dlp2',
+              name:'photoshop商业网站设计',
+            },
+            {
+              id:'dlp3',
+              name:'C#程序设计',
+            },
+            {
+              id:'dlp4',
+              name:'sql serve数据库基础',
+            },
+            {
+              id:'dlp5',
+              name:'C#桌面应用程序开发',
+            },
+            {
+              id:'dlp6',
+              name:'java语言基础',
+            },
+            {
+              id:'dlp7',
+              name:'java面对对象编程',
+            },
+            {
+              id:'dlp8',
+              name:'java语言高级特性',
+            },
+          ]
+        },
+        {
+          id:2,
+          title:'移动互联网应用专业书籍',
+          books:[
+            {
+              id:'apl1',
+              name:'计算机基础应用',
+            },
+            {
+              id:'apl2',
+              name:'linux系统管理',
+            },
+            {
+              id:'apl3',
+              name:'企业网络互联网技术',
+            },
+            {
+              id:'apl4',
+              name:'windows网络服务管理',
+            },
+            {
+              id:'apl5',
+              name:'windows系统管理',
+            },
+            {
+              id:'apl6',
+              name:'windows活动目录管理',
+            },
+            {
+              id:'apl7',
+              name:'计算机网络技术',
+            },
+          ]
+        }
+      ],
+      //卡片
+      cards:[
+        {
+          id:1,
+          title:'移动互联网开发全套书籍',
+          img:'',
+          text:'  与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；在界面中一致：所有的元素和结构需保持一致。'
+        },
+        {
+          id:2,
+          title:'移动互联网应用全套书籍',
+          img:'',
+          text:'与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；在界面中一致：所有的元素和结构需保持一致.'
+        },
+        {
+          id:3,
+          title:'大数据全套书籍',
+          img:'',
+          text:'与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；在界面中一致：所有的元素和结构需保持一致。'
+        },
+        {
+          id:4,
+          title:'人工智能全套书籍',
+          img:'',
+          text:'与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；在界面中一致：所有的元素和结构需保持一致。'
+        },
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+ .tit{
+  height: 40px;
+  line-height: 40px;
+  color: #fff;
+  background: navy;
+
+  }
+.ltBox{
+  background: #fff;
+  margin-bottom: 3px;
+  .bookMenu{
+    height: 30px;
+    line-height: 30px;
+    font-size: 16px;
+  }
+}
+.img{
+  height: 300px;
+  background: orangered;
+}
+.buybtn{
+  margin: 10px 0 0 10px;
+}
+</style>
